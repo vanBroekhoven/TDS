1deb1f30cb5ffad870c51419fd5ddba4f18f3e90
Roy Buitenhuis <roy.buitenhuis94@gmail.com>
2017-09-18 11:17:22 +0200

    oops, added additional folders that didn't belong here.

--- blinky/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibc.a -l"/home/roy/ti/TivaWare_C_Series-2.1.3.156/driverlib/ccs/Debug/driverlib.lib"
-
